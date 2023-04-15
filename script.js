667af43c2c18c84d74beb18279990e30e2dcb714
HendersonSoftworks <thomas.henderson@hendersonsoftworks.com>
2023-04-15 18:52:42 +0100

    feat: created monsters.json file

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+let monsters = [];
+let last_grid_el;
+
+async function GetMonsters()
+{
+    let response = await fetch('monsters.json');
+    let data = await response.json();
+    return data;
+}
+
+function PopulateSelectPanel(json)
+{
+    for (let i = 0; i < json.length; i++) {
+        monsters[i] = json[i];
+    }
+
+    // this isn't working
+    for (let i = 0; i < monsters.length; i++) {
+        last_grid_el = $('.monster-select-panel').clone();
+        last_grid_el.find("span")[0].innerHTML = monsters[i].name;
+    
+        $(last_grid_el).appendTo(".monster-modal");        
+    }
+
+   
+}
+
 // populate sample goblins
 for (let i = 0; i < 2; i++) 
 {
@@ -6,11 +33,11 @@ for (let i = 0; i < 2; i++)
 }
 
 // populate selection modoal
-for (let i = 0; i < 4; i++) 
-{
-    let last_grid_el = $('.monster-select-panel').clone();
-    $(last_grid_el).appendTo(".monster-modal");
-}
+// for (let i = 0; i < 4; i++) 
+// {
+//     let last_grid_el = $('.monster-select-panel').clone();
+//     $(last_grid_el).appendTo(".monster-modal");
+// }
 
 $(".add-btn").click(function() {
     $(".monster-modal").toggle()
@@ -21,3 +48,7 @@ $(".monster-select-panel").click(function() {
     console.log("adding monster to manager panel...")
 });
 
+GetMonsters()
+    .then(data => (monsters = data))
+    //.then(monsters => (PopulateSelectPanel(monsters)));
+    .then(monsters => (PopulateSelectPanel(monsters)));
