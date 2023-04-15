cf6b9551e33e6b14aca8ffac4d635767e7c0c102
HendersonSoftworks <thomas.henderson@hendersonsoftworks.com>
2023-04-15 16:34:12 +0100

    feat: added monster selection panel

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+// populate sample goblins
 for (let i = 0; i < 2; i++) 
 {
     let last_grid_el = $('.enemy-panel').clone();
     $(last_grid_el).appendTo(".grid-container");
 }
+
+// populate selection modoal
+for (let i = 0; i < 4; i++) 
+{
+    let last_grid_el = $('.monster-select-panel').clone();
+    $(last_grid_el).appendTo(".monster-modal");
+}
+
+$(".add-btn").click(function() {
+    $(".monster-modal").toggle()
+});
+
+$(".monster-select-panel").click(function() {
+    $(".monster-modal").toggle()
+    console.log("adding monster to manager panel...")
+});
+
